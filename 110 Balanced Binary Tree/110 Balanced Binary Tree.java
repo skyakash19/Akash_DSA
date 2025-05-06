f25def66286b3b40e442753f1b41a878856a7385
Akash R A <akashrelekar1904@gmail.com>
2025-05-06 22:04:37 +0530

    Update 110 Balanced Binary Tree.java

--- 110 Balanced Binary Tree/110 Balanced Binary Tree.java
+++ 110 Balanced Binary Tree/110 Balanced Binary Tree.java
@@ -1,25 +1,3 @@
-// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
-
-// Leetcode Link: https://leetcode.com/problems/balanced-binary-tree/
-
-// Video Solution: https://www.youtube.com/watch?v=gu5rAEvm9Fk
-
-**************** Java Solution ***********************
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public boolean isBalanced(TreeNode root) {
         if (root == null) {
