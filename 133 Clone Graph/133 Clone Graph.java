f698215d53bce55a3654cac5d1013ec23b380a08
Akash R A <akashrelekar1904@gmail.com>
2025-05-06 22:26:00 +0530

    Update 133 Clone Graph.java

--- 133 Clone Graph/133 Clone Graph.java
+++ 133 Clone Graph/133 Clone Graph.java
@@ -1,23 +1,3 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public List<Node> neighbors;
-    public Node() {
-        val = 0;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val) {
-        val = _val;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val, ArrayList<Node> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-}
-*/
-
 class Solution {
     
     HashMap<Node, Node> visited = new HashMap<>();
@@ -42,20 +22,6 @@ class Solution {
         return cloneNode;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
