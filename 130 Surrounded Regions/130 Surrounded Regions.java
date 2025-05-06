7a7086562d408c038b3f3de65f0691536dce4fca
Akash R A <akashrelekar1904@gmail.com>
2025-05-06 22:23:18 +0530

    Update 130 Surrounded Regions.java

--- 130 Surrounded Regions/130 Surrounded Regions.java
+++ 130 Surrounded Regions/130 Surrounded Regions.java
@@ -1,17 +1,8 @@
-// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
-
-// Leetcode Link: https://leetcode.com/problems/surrounded-regions/
-
-// Video Solution: https://www.youtube.com/watch?v=yaBaE4fo6wA
-
-**************** Java Solution ***********************
-
 public class Solution {
     public void solve(char[][] board) {
         if (board == null || board.length == 0) return;
         int m = board.length, n = board[0].length;
 
-        // Mark boundary-connected 'O's with 'T'
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if ((i == 0 || i == m - 1 || j == 0 || j == n - 1) && board[i][j] == 'O') {
@@ -20,7 +11,6 @@ public class Solution {
             }
         }
 
-        // Capture surrounded regions and revert 'T' to 'O'
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (board[i][j] == 'O') {
@@ -36,7 +26,7 @@ public class Solution {
         if (i < 0 || i >= board.length || j < 0 || j >= board[i].length || board[i][j] != 'O') {
             return;
         }
-        board[i][j] = 'T'; // Mark as visited
+        board[i][j] = 'T'; 
         dfs(board, i + 1, j);
         dfs(board, i - 1, j);
         dfs(board, i, j + 1);
