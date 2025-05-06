09878312cd4e63da66fefffae45228ca5d0cec45
Akash R A <akashrelekar1904@gmail.com>
2025-05-06 21:56:51 +0530

    Update 103 Binary Tree Zigzag Level Order Traversal.java

--- 103 Binary Tree Zigzag Level Order Traversal/103 Binary Tree Zigzag Level Order Traversal.java
+++ 103 Binary Tree Zigzag Level Order Traversal/103 Binary Tree Zigzag Level Order Traversal.java
@@ -1,21 +1,3 @@
-Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
-
-Leetcode Link:https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
-
-Video Solution: https://www.youtube.com/watch?v=Oy3g4SEKNw0
-
-**************** Java Solution ***********************
-
-
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 class Solution {
   public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
     if (root == null) {
@@ -23,8 +5,6 @@ class Solution {
     }
 
     List<List<Integer>> results = new ArrayList<List<Integer>>();
-
-    // add the root element with a delimiter to kick off the BFS loop
     LinkedList<TreeNode> node_queue = new LinkedList<TreeNode>();
     node_queue.addLast(root);
     node_queue.addLast(null);
@@ -44,13 +24,10 @@ class Solution {
           node_queue.addLast(curr_node.left);
         if (curr_node.right != null)
           node_queue.addLast(curr_node.right);
-
       } else {
-        // we finish the scan of one level
         results.add(level_list);
         level_list = new LinkedList<Integer>();
-        // prepare for the next level
-        if (node_queue.size() > 0)
+       if (node_queue.size() > 0)
           node_queue.addLast(null);
         is_order_left = !is_order_left;
       }
