26fb8720074aa900a1390b2b2b4fae32b0e1902c
Akash R A <akashrelekar1904@gmail.com>
2025-05-06 21:50:52 +0530

    Update 101 Symmetric Tree.java

--- 101 Symmetric Tree/101 Symmetric Tree.java
+++ 101 Symmetric Tree/101 Symmetric Tree.java
@@ -1,11 +1,3 @@
-// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
-
-// Leetcode Link: https://leetcode.com/problems/symmetric-tree/
-
-// Video Solution: https://www.youtube.com/watch?v=NeSeH2ECZUw
-
-**************** Java Solution ***********************
-
 class TreeNode {
     int val;
     TreeNode left;
