1d7ca438613387d53825a3584bb63d1c79aca2b5
Akash R A <akashrelekar1904@gmail.com>
2025-05-06 22:10:07 +0530

    Update 118 Pascal's Triangle.java

--- 118 Pascal's Triangle/118 Pascal's Triangle.java
+++ 118 Pascal's Triangle/118 Pascal's Triangle.java
@@ -1,10 +1,3 @@
-// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
-//Leetcode Link: https://leetcode.com/problems/pascals-triangle/
-//Video Solution: https://www.youtube.com/watch?v=f0EEHwaY3HY
-
-//**************** Java Solution ***********************
-
-
 class Solution {
     public List<List<Integer>> generate(int numRows) {
 
