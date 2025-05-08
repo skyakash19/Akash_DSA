07aad055f0897820a14e943af1aceec8fdfc376f
Akash R A <akashrelekar1904@gmail.com>
2025-05-09 00:57:06 +0530

    Update 2 Add Two Numbers.java

--- 2 Add Two Numbers/2 Add Two Numbers.java
+++ 2 Add Two Numbers/2 Add Two Numbers.java
@@ -1,22 +1,3 @@
-Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
-
-Leetcode Link: https://leetcode.com/problems/add-two-numbers/
-
-Video Solution: https://www.youtube.com/watch?v=MYtp__JpTns
-
-**************** Java Solution ***********************
-
-
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(0);
@@ -47,6 +28,3 @@ class Solution {
         
     }
 }
-
-
-//test
