6fe361cc5c6109c4c4219e22bba7adbf7b348880
Akash R A <akashrelekar1904@gmail.com>
2025-05-09 00:13:32 +0530

    Update 14 Longest Common Prefix.java

--- 14 Longest Common Prefix/14 Longest Common Prefix.java
+++ 14 Longest Common Prefix/14 Longest Common Prefix.java
@@ -1,11 +1,3 @@
-// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
-
-// Leetcode Link: https://leetcode.com/problems/longest-common-prefix/
-
-// Video Solution: https://www.youtube.com/watch?v=PWoIZxcamsQ
-
-**************** Java Solution ***********************
-
 public class Solution {
     public String longestCommonPrefix(String[] strs) {
         if (strs == null || strs.length == 0) return "";
