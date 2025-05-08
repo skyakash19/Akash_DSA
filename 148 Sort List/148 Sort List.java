27115fac2341adeb86edbb6dce65a9afed74d0fb
Akash R A <akashrelekar1904@gmail.com>
2025-05-09 00:22:39 +0530

    Update 148 Sort List.java

--- 148 Sort List/148 Sort List.java
+++ 148 Sort List/148 Sort List.java
@@ -1,41 +1,27 @@
-// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
-
-// Leetcode Link: https://leetcode.com/problems/sort-list/
-
-// Video Solution: https://www.youtube.com/watch?v=7halZ77R55o
-
-**************** Java Solution ***********************
-
-
 public class Solution {
     public ListNode sortList(ListNode head) {
         if (head == null || head.next == null) {
             return head;
         }
 
-        // Split the list into two halves
         ListNode mid = getMid(head);
-        ListNode left = sortList(head); // sort the first half
-        ListNode right = sortList(mid); // sort the second half
-
-        // Merge the sorted halves
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
         return merge(left, right);
     }
-
-    // Function to find the middle of the list
-    private ListNode getMid(ListNode head) {
+    
+        private ListNode getMid(ListNode head) {
         ListNode prev = null;
         while (head != null && head.next != null) {
             prev = (prev == null) ? head : prev.next;
             head = head.next.next;
         }
         ListNode mid = prev.next;
-        prev.next = null; // Split the list into two halves
+        prev.next = null; 
         return mid;
     }
 
-    // Function to merge two sorted lists
-    private ListNode merge(ListNode list1, ListNode list2) {
+        private ListNode merge(ListNode list1, ListNode list2) {
         ListNode dummy = new ListNode(0);
         ListNode tail = dummy;
 
@@ -50,7 +36,7 @@ public class Solution {
             tail = tail.next;
         }
 
-        // Append the remaining nodes of list1 or list2
+       
         tail.next = (list1 != null) ? list1 : list2;
 
         return dummy.next;
