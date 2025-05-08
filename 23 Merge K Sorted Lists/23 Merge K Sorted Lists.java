9e8bf1df231afd2e6ffcb7e4b57eb4a001b5433b
Akash R A <akashrelekar1904@gmail.com>
2025-05-09 01:15:32 +0530

    Update 23 Merge K Sorted Lists.java

--- 23 Merge K Sorted Lists/23 Merge K Sorted Lists.java
+++ 23 Merge K Sorted Lists/23 Merge K Sorted Lists.java
@@ -1,16 +1,3 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-// LeetCode Problem Link: https://leetcode.com/problems/merge-k-sorted-lists/0
-//Youtube Solution: https://youtu.be/SlKgDtpJnxg
-
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         
