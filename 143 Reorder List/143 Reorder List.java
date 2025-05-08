e0c53b00bb7895aa1c6ea0844376fc482a1120d5
Akash R A <akashrelekar1904@gmail.com>
2025-05-09 00:15:10 +0530

    Update 143 Reorder List.java

--- 143 Reorder List/143 Reorder List.java
+++ 143 Reorder List/143 Reorder List.java
@@ -1,16 +1,3 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-
-// LeetCode Link: https://leetcode.com/problems/reorder-list/
-// Youtube Explaination: https://youtu.be/lBdS4AV1EGw
 class Solution {
     public void reorderList(ListNode head) {
         
