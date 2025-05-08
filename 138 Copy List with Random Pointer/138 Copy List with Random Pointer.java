7fa6c42c3141f9c0aacd20bb8b81959fc6d36239
Akash R A <akashrelekar1904@gmail.com>
2025-05-09 00:12:28 +0530

    Update 138 Copy List with Random Pointer.java

--- 138 Copy List with Random Pointer/138 Copy List with Random Pointer.java
+++ 138 Copy List with Random Pointer/138 Copy List with Random Pointer.java
@@ -1,27 +1,3 @@
-// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
-
-// Leetcode Link: https://leetcode.com/problems/copy-list-with-random-pointer/
-
-// Video Solution: https://www.youtube.com/watch?v=vy7ZJ4TdyS8
-
-// **************** Java Solution ***********************
-
-
-/*
-// Definition for a Node.
-class Node {
-    int val;
-    Node next;
-    Node random;
-
-    public Node(int val) {
-        this.val = val;
-        this.next = null;
-        this.random = null;
-    }
-}
-*/
-
 class Solution {
     
     HashMap<Node, Node> visitedNode = new HashMap<Node, Node>();
