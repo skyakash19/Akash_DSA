b56e85f68efe5dcc00b24101290e300fd6c7b2c2
Akash R A <akashrelekar1904@gmail.com>
2025-05-09 00:24:20 +0530

    Update 150 Evaluate Reverse Polish Notation.java

--- 150 Evaluate Reverse Polish Notation/150 Evaluate Reverse Polish Notation.java
+++ 150 Evaluate Reverse Polish Notation/150 Evaluate Reverse Polish Notation.java
@@ -1,11 +1,3 @@
-// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
-
-// Leetcode Link:  https://leetcode.com/problems/evaluate-reverse-polish-notation/
-
-// Video Solution: https://www.youtube.com/watch?v=vDRZN5i4b8U
-
-**************** Java Solution ***********************
-
 import java.util.Stack;
 
 public class Solution {
@@ -14,8 +6,8 @@ public class Solution {
 
         for (String token : tokens) {
             if (isOperator(token)) {
-                int b = stack.pop(); // Second operand
-                int a = stack.pop(); // First operand
+                int b = stack.pop();
+                int a = stack.pop(); 
                 int result = applyOperator(token, a, b);
                 stack.push(result);
             } else {
