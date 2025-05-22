8a83507b2087afe2fb5b5145a18368d7c1f09888
Akash R A <akashrelekar1904@gmail.com>
2025-05-22 16:16:36 +0530

    Update 84 Largest Rectangle in Histogram.java

--- 84 Largest Rectangle in Histogram/84 Largest Rectangle in Histogram.java
+++ 84 Largest Rectangle in Histogram/84 Largest Rectangle in Histogram.java
@@ -1,11 +1,3 @@
-// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
-
-// Leetcode Link:  https://leetcode.com/problems/largest-rectangle-in-histogram/
-
-// Video Solution: https://www.youtube.com/watch?v=IasMlShanvc
-
-**************** Java Solution ***********************
-
 class Solution {
     public int largestRectangleArea(int[] heights) {
         int maxArea = 0;
