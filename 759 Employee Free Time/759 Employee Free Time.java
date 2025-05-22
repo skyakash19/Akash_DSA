e99ee444f3beb10bf9220f75a9b914059a3e9a3e
Akash R A <akashrelekar1904@gmail.com>
2025-05-22 16:09:06 +0530

    Update 759 Employee Free Time.java

--- 759 Employee Free Time/759 Employee Free Time.java
+++ 759 Employee Free Time/759 Employee Free Time.java
@@ -1,12 +1,3 @@
-// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
-
-// Leetcode Link: https://leetcode.ca/all/759.html
-
-// Video Solution: https://www.youtube.com/watch?v=99l7goR4y0U
-
-**************** Java Solution ***********************
-
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.PriorityQueue;
@@ -21,12 +12,11 @@ class Interval {
     }
 }
 
-class Solution { // Removed the 'public' modifier
+class Solution {
     public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
         List<Interval> result = new ArrayList<>();
         PriorityQueue<Interval> pq = new PriorityQueue<>((a, b) -> a.start - b.start);
 
-        // Add all intervals to the priority queue
         for (List<Interval> intervals : schedule) {
             pq.addAll(intervals);
         }
@@ -35,11 +25,11 @@ class Solution { // Removed the 'public' modifier
         while (!pq.isEmpty()) {
             Interval curr = pq.poll();
             if (prev.end < curr.start) {
-                // There is a gap between prev and curr, which is a common free time
+
                 result.add(new Interval(prev.end, curr.start));
                 prev = curr;
             } else {
-                // Overlapping intervals, update the end time if needed
+
                 prev.end = Math.max(prev.end, curr.end);
             }
         }
