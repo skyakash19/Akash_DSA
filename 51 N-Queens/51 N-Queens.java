1c8b9dbbfb56276547ced8f11ff08bef639578d1
Akash R A <akashrelekar1904@gmail.com>
2025-05-22 15:12:58 +0530

    Update 51 N-Queens.java

--- 51 N-Queens/51 N-Queens.java
+++ 51 N-Queens/51 N-Queens.java
@@ -1,11 +1,3 @@
-// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
-
-// Leetcode Link: https://leetcode.com/problems/n-queens/
-
-// Video Solution: https://www.youtube.com/watch?v=MOrMPEq6P5w
-
-**************** Java Solution ***********************
-
 class Solution {
     public List<List<String>> solveNQueens(int n) {
         char[][] board = new char[n][n];
