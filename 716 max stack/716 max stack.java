22339d79ef3691f61e1071b98b5f37a5ff1ef510
Akash R A <akashrelekar1904@gmail.com>
2025-05-22 15:31:43 +0530

    Update 716 max stack.java

--- 716 max stack/716 max stack.java
+++ 716 max stack/716 max stack.java
@@ -1,11 +1,3 @@
-// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
-
-// Leetcode Link:  https://leetcode.ca/all/716.html
-
-// Video Solution: https://www.youtube.com/watch?v=SyDmmNQFW_I
-
-
-**************** Java Solution ***********************
 import java.util.Stack;
 
 class MaxStack {
