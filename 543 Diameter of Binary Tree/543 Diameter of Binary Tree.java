568c97aeebe3b659f793a3f9330634b37ad4b42a
Akash R A <akashrelekar1904@gmail.com>
2025-05-22 15:16:23 +0530

    Update 543 Diameter of Binary Tree.java

--- 543 Diameter of Binary Tree/543 Diameter of Binary Tree.java
+++ 543 Diameter of Binary Tree/543 Diameter of Binary Tree.java
@@ -1,29 +1,5 @@
-// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
-
-// Leetcode Link: https://leetcode.com/problems/diameter-of-binary-tree/
-
-// Video Solution: https://www.youtube.com/watch?v=DpyCkHMlxLc
-
-**************** Java Solution ***********************
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-
-
 class Solution {
-    int maxDiameter = 0; // Global variable to keep track of the maximum diameter
+    int maxDiameter = 0;
 
     public int diameterOfBinaryTree(TreeNode root) {
         getHeight(root);
@@ -31,15 +7,13 @@ class Solution {
     }
 
     private int getHeight(TreeNode node) {
-        if (node == null) return 0; // Base case
+        if (node == null) return 0;
 
-        int leftHeight = getHeight(node.left); // Height of left subtree
-        int rightHeight = getHeight(node.right); // Height of right subtree
+        int leftHeight = getHeight(node.left); 
+        int rightHeight = getHeight(node.right);
 
-        // Update the maximum diameter if needed
         maxDiameter = Math.max(maxDiameter, leftHeight + rightHeight);
 
-        // Return the height of the current node
         return 1 + Math.max(leftHeight, rightHeight);
     }
 }
