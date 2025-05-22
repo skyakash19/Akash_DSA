d6bc7b31df1447c9a1ce6d2157b020f51cf1b744
Akash R A <akashrelekar1904@gmail.com>
2025-05-22 16:16:11 +0530

    Update 818 Race Car.java

--- 818 Race Car/818 Race Car.java
+++ 818 Race Car/818 Race Car.java
@@ -1,12 +1,3 @@
-// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
-
-// Leetcode Link: https://leetcode.com/problems/race-car/
-
-// Video Solution: https://www.youtube.com/watch?v=TAXt_TkSNfw
-
-**************** Java Solution ***********************
-
-
 class Solution {
     public int racecar(int target) {
         
