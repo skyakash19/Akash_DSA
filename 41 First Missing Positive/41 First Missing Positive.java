fe40c759e308ff481df2d8a9480717fff173e6f4
Akash R A <akashrelekar1904@gmail.com>
2025-05-22 15:02:43 +0530

    Update 41 First Missing Positive.java

--- 41 First Missing Positive/41 First Missing Positive.java
+++ 41 First Missing Positive/41 First Missing Positive.java
@@ -1,12 +1,3 @@
-Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
-
-Leetcode Link: https://leetcode.com/problems/first-missing-positive/
-
-Video Solution: https://www.youtube.com/watch?v=N8DefKVUIKw
-
-**************** Java Solution ***********************
-
-
 class Solution {
     public int firstMissingPositive(int[] nums) {
         
