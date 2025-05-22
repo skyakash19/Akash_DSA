7daa261255e365afa7897b629f90eb34bceb3480
Akash R A <akashrelekar1904@gmail.com>
2025-05-22 16:14:39 +0530

    Update 78 Subsets.java

--- 78 Subsets/78 Subsets.java
+++ 78 Subsets/78 Subsets.java
@@ -1,12 +1,3 @@
-// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
-
-// Leetcode Link: https://leetcode.com/problems/subsets/
-
-// Video Solution: https://www.youtube.com/watch?v=OQKWzRJYtwg
-
-**************** Java Solution ***********************
-
-
 class Solution {
      public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
