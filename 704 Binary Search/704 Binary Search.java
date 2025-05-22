12d5948360e999dda78aec6ad371bf9c9d585276
Akash R A <akashrelekar1904@gmail.com>
2025-05-22 15:31:09 +0530

    Update 704 Binary Search.java

--- 704 Binary Search/704 Binary Search.java
+++ 704 Binary Search/704 Binary Search.java
@@ -1,11 +1,3 @@
-// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
-
-// Leetcode Link:  https://leetcode.com/problems/binary-search/
-
-// Video Solution: https://www.youtube.com/watch?v=t3yPHFyKxr4
-
-
-**************** Java Solution ***********************
 class Solution {
     public int search(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
@@ -22,6 +14,6 @@ class Solution {
             }
         }
         
-        return -1; // Target not found
+        return -1;
     }
 }
