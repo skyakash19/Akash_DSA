41ff8fee5538132ec95690475f0fd886f6ecfbe8
Akash R A <akashrelekar1904@gmail.com>
2025-05-22 16:18:44 +0530

    Update 875 Koko Eating Bananas.java

--- 875 Koko Eating Bananas/875 Koko Eating Bananas.java
+++ 875 Koko Eating Bananas/875 Koko Eating Bananas.java
@@ -1,11 +1,3 @@
-// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
-
-// Leetcode Link: https://leetcode.com/problems/koko-eating-bananas/
-
-// Video Solution: https://www.youtube.com/watch?v=3m3QZeiH3KI
-
-
-**************** Java Solution ***********************
 class Solution {
    public static int minEatingSpeed(int[] piles, int h) {
         int left = 1, right = 1;
