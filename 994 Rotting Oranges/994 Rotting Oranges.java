e5c2ee04acea1e186b21dfe513d4a20a2c36120a
Akash R A <akashrelekar1904@gmail.com>
2025-05-22 16:26:17 +0530

    Update 994 Rotting Oranges.java

--- 994 Rotting Oranges/994 Rotting Oranges.java
+++ 994 Rotting Oranges/994 Rotting Oranges.java
@@ -1,11 +1,3 @@
-// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
-
-// Leetcode Link: https://leetcode.com/problems/rotting-oranges/
-
-// Video Solution: https://www.youtube.com/watch?v=1BlwbFfgk-E
-
-**************** Java Solution ***********************
-
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -17,7 +9,6 @@ public class Solution {
         int freshCount = 0;
         Queue<int[]> rottenQueue = new LinkedList<>();
 
-        // Count fresh oranges and enqueue rotten oranges
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 1) {
@@ -28,12 +19,9 @@ public class Solution {
             }
         }
 
-        if (freshCount == 0) return 0; // No fresh oranges
-
+        if (freshCount == 0) return 0;
         int minutes = 0;
-        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}}; // 4-directional
-
-        // BFS
+        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}}; 
         while (!rottenQueue.isEmpty()) {
             int size = rottenQueue.size();
             for (int i = 0; i < size; i++) {
@@ -42,16 +30,15 @@ public class Solution {
                     int x = rotten[0] + dir[0];
                     int y = rotten[1] + dir[1];
                     if (x >= 0 && x < m && y >= 0 && y < n && grid[x][y] == 1) {
-                        grid[x][y] = 2; // Turn fresh orange to rotten
+                        grid[x][y] = 2; 
                         freshCount--;
                         rottenQueue.offer(new int[]{x, y});
                     }
                 }
             }
-            minutes++; // Increment timer after each level of BFS
+            minutes++; 
         }
 
-        return freshCount == 0 ? minutes - 1 : -1; // Adjust for extra increment
+        return freshCount == 0 ? minutes - 1 : -1;
     }
 }
-
