36dedb27dc33b00760e1ad4c6ef370824990e5a0
Akash R A <akashrelekar1904@gmail.com>
2025-05-22 14:37:38 +0530

    Update 309 Best Time to Buy and Sell Stock with Cooldown.java

--- 309 Best Time to Buy and Sell Stock with Cooldown/309 Best Time to Buy and Sell Stock with Cooldown.java
+++ 309 Best Time to Buy and Sell Stock with Cooldown/309 Best Time to Buy and Sell Stock with Cooldown.java
@@ -1,11 +1,3 @@
-// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
-
-// Leetcode Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
-
-// Video Solution: https://www.youtube.com/watch?v=PH5jUN1cNHo
-
-**************** Java Solution ***********************
-
 class Solution {
     public int maxProfit(int[] prices) {
         if (prices == null || prices.length <= 1) return 0;
@@ -15,9 +7,9 @@ class Solution {
         int[] sell = new int[n];
         int[] rest = new int[n];
 
-        buy[0] = -prices[0];  // Buying on the first day
-        sell[0] = 0;          // Cannot sell on the first day
-        rest[0] = 0;          // No profit on the first day if we rest
+        buy[0] = -prices[0];
+        sell[0] = 0;
+        rest[0] = 0;
 
         for (int i = 1; i < n; i++) {
             buy[i] = Math.max(buy[i-1], rest[i-1] - prices[i]);
@@ -25,6 +17,6 @@ class Solution {
             rest[i] = Math.max(rest[i-1], sell[i-1]);
         }
 
-        return Math.max(sell[n-1], rest[n-1]); // Maximum profit on the last day, after all transactions
+        return Math.max(sell[n-1], rest[n-1]);
     }
 }
