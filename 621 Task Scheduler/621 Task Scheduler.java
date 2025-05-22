cced08467fe83328f72140a20cb5f706b16333bb
Akash R A <akashrelekar1904@gmail.com>
2025-05-22 15:20:18 +0530

    Update 621 Task Scheduler.java

--- 621 Task Scheduler/621 Task Scheduler.java
+++ 621 Task Scheduler/621 Task Scheduler.java
@@ -1,25 +1,14 @@
-// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
-
-// Leetcode Link: https://leetcode.com/problems/task-scheduler/
-
-// Video Solution: https://www.youtube.com/watch?v=IcjlqToRGWI
-
-**************** Java Solution ***********************
-
-
 class Solution {
      public int leastInterval(char[] tasks, int n) {
-        // Step 1: Count the frequency of each task
+
         Map<Character, Integer> freqMap = new HashMap<>();
         for (char task : tasks) {
             freqMap.put(task, freqMap.getOrDefault(task, 0) + 1);
         }
 
-        // Step 2: Build a max heap based on the frequency
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
         maxHeap.addAll(freqMap.values());
 
-        // Step 3: Process tasks
         int time = 0;
         while (!maxHeap.isEmpty()) {
             List<Integer> temp = new ArrayList<>();
@@ -35,7 +24,6 @@ class Solution {
                 }
             }
 
-            // Step 4: Update time
             time += maxHeap.isEmpty() ? temp.size() : n + 1;
         }
 
