665482858657d0e2483a34a74f1fff780e4ccc1d
Akash R A <akashrelekar1904@gmail.com>
2025-05-22 14:58:31 +0530

    Update 371 Sum of Two Integers.java

--- 371 Sum of two integers/371 Sum of Two Integers.java
+++ 371 Sum of two integers/371 Sum of Two Integers.java
@@ -1,8 +1,6 @@
 class Solution {
     public int getSum(int a, int b) {
         
-        //int carry = a&b << 1;
-        
         while(b != 0){
             int temp = a ^ b;
             int carry = (a & b) << 1;
