ae506bc86bf60ada583f0cdd59c2f87051da4ed5
Akash R A <akashrelekar1904@gmail.com>
2025-05-22 15:08:56 +0530

    Update 46 Permutations.java

--- 46 permutations/46 Permutations.java
+++ 46 permutations/46 Permutations.java
@@ -1,12 +1,3 @@
-// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
-
-// Leetcode Link: https://leetcode.com/problems/permutations/
-
-// Video Solution: https://www.youtube.com/watch?v=_tgwDDWuU-Q
-
-**************** Java Solution ***********************
-
-
 import java.util.ArrayList;
 import java.util.List;
 
