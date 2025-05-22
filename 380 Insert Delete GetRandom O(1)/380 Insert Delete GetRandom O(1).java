e7d6408dd37c27e9943a180431fec8b8b2b97720
Akash R A <akashrelekar1904@gmail.com>
2025-05-22 14:59:26 +0530

    Update 380 Insert Delete GetRandom O(1).java

--- 380 Insert Delete GetRandom O(1)/380 Insert Delete GetRandom O(1).java
+++ 380 Insert Delete GetRandom O(1)/380 Insert Delete GetRandom O(1).java
@@ -1,12 +1,3 @@
-// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
-
-// Leetcode Link: https://leetcode.com/problems/insert-delete-getrandom-o1/
-
-// Video Solution: https://www.youtube.com/watch?v=cRPoqZOlDkg
-
-**************** Java Solution ***********************
-
-
 import java.util.*;
 
 public class RandomizedSet {
@@ -36,11 +27,9 @@ public class RandomizedSet {
         int index = map.get(val);
         int lastElement = list.get(list.size() - 1);
 
-        // Swap the last element with the element to be removed
         list.set(index, lastElement);
         map.put(lastElement, index);
-
-        // Remove the last element
+ 
         list.remove(list.size() - 1);
         map.remove(val);
 
