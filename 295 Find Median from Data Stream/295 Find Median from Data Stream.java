651eb0dfa7872d920e913a0e7c0c354a4ee011f0
Akash R A <akashrelekar1904@gmail.com>
2025-05-22 14:34:44 +0530

    Update 295 Find Median from Data Stream.java

--- 295 Find Median from Data Stream/295 Find Median from Data Stream.java
+++ 295 Find Median from Data Stream/295 Find Median from Data Stream.java
@@ -1,31 +1,22 @@
-// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
-
-// Leetcode Link:https://leetcode.com/problems/find-median-from-data-stream/
-
-// Video Solution: https://www.youtube.com/watch?v=IKpM6Q8wTIY
-
-**************** Java Solution ***********************
-
 import java.util.PriorityQueue;
 
 public class MedianFinder {
 
-    private PriorityQueue<Integer> lo = new PriorityQueue<>((a, b) -> b - a);  // max heap
-    private PriorityQueue<Integer> hi = new PriorityQueue<>();  // min heap
+    private PriorityQueue<Integer> lo = new PriorityQueue<>((a, b) -> b - a);
+    private PriorityQueue<Integer> hi = new PriorityQueue<>(); 
 
-    // Adds a number into the data structure.
+
     public void addNum(int num) {
-        lo.offer(num);  // Add to max heap
+        lo.offer(num);
 
-        hi.offer(lo.poll());  // balancing step
+        hi.offer(lo.poll());
 
-        if (lo.size() < hi.size()) {  // maintain size property
+        if (lo.size() < hi.size()) { 
             lo.offer(hi.poll());
         }
     }
 
-    // Returns the median of current data stream
-    public double findMedian() {
+     public double findMedian() {
         return lo.size() > hi.size() ? lo.peek() : (lo.peek() + hi.peek()) * 0.5;
     }
 }
