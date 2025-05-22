53ead4587519696dd224263067e2475b2f0fc989
Akash R A <akashrelekar1904@gmail.com>
2025-05-22 14:58:02 +0530

    Update 366 Find Leaves of Binary Tree.java

--- 366 Find Leaves of Binary Tree/366 Find Leaves of Binary Tree.java
+++ 366 Find Leaves of Binary Tree/366 Find Leaves of Binary Tree.java
@@ -1,12 +1,3 @@
-// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
-
-// Leetcode Link: https://leetcode.ca/all/366.html
-
-// Video Solution: https://www.youtube.com/watch?v=VZCWLZ5PvKM
-
-**************** Java Solution ***********************
-
-
 import java.util.ArrayList;
 import java.util.List;
 
