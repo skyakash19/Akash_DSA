c1e674c20e0de5ca8ac5b18a157df0e17ed4b285
Akash R A <akashrelekar1904@gmail.com>
2025-05-22 16:24:55 +0530

    Update 981 Time Based Key-Value Store.java

--- 981 Time Based Key-Value Store/981 Time Based Key-Value Store.java
+++ 981 Time Based Key-Value Store/981 Time Based Key-Value Store.java
@@ -1,11 +1,3 @@
-// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
-
-// Leetcode Link: https://leetcode.com/problems/time-based-key-value-store/
-
-// Video Solution: https://www.youtube.com/watch?v=dP4cb8zbohY
-
-
-**************** Java Solution ***********************
 class TimeMap {
 private Map<String, TreeMap<Integer, String>> map;
 
@@ -26,10 +18,3 @@ private Map<String, TreeMap<Integer, String>> map;
         return entry == null ? "" : entry.getValue();
     }
 }
-
-/**
- * Your TimeMap object will be instantiated and called as such:
- * TimeMap obj = new TimeMap();
- * obj.set(key,value,timestamp);
- * String param_2 = obj.get(key,timestamp);
- */
