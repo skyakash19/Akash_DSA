4aca488d4fa309f1415310cf3ac4dd786a96b920
Akash R A <akashrelekar1904@gmail.com>
2025-05-22 14:23:40 +0530

    Update 271 Encode and Decode Strings.java

--- 271 Encode and Decode Strings/271 Encode and Decode Strings.java
+++ 271 Encode and Decode Strings/271 Encode and Decode Strings.java
@@ -1,9 +1,5 @@
-//Leetcode Link: https://leetcode.com/problems/encode-and-decode-strings/
-//Youtube Link: 
-
 public class Codec {
 
-    // Encodes a list of strings to a single string.
     public String encode(List<String> strs) {
         
         if(strs.size()==0 ){
@@ -20,7 +16,6 @@ public class Codec {
         
     }
 
-    // Decodes a single string to a list of strings.
     public List<String> decode(String s) {
         
         if(s.equals(Character.toString((char)258))){
@@ -32,7 +27,3 @@ public class Codec {
          return Arrays.asList(s.split(separate, -1));
     }
 }
-
-// Your Codec object will be instantiated and called as such:
-// Codec codec = new Codec();
-// codec.decode(codec.encode(strs));
